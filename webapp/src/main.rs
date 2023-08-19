0af362e0d22e34a9b722d7af7c94abe81946b4ef
Konstantin-Dudersky <Konstantin.Dudersky@gmail.com>
2023-08-19 20:29:23 +0300

    add webapp

---
+++ webapp/src/main.rs
@@ -0,0 +1,8 @@
+use leptos::*;
+mod log;
+
+pub fn main() {
+    // log::init();
+    // tracing::event!(log::Level::WARN, "start webapp");
+    mount_to_body(|| view! {  <p>"fsfsfd dfdg df g"</p>})
+}
